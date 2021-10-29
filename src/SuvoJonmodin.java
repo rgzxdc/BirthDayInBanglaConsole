f7eb507d3913a60529c7e1249c6f398113b5984c
Shayed Hasan Shakib <shayedhasanshakib@gmail.com>
2021-10-29 07:12:06 +0600

    made 10 different letter-case from three string for time delay letter by letter

--- src/SuvoJonmodin.java
+++ src/SuvoJonmodin.java
@@ -5,15 +5,37 @@ public class SuvoJonmodin {
     public static void main(String[] args) throws InterruptedException {
 
 
-        String happy = "\u09B6\u09C1\u09AD";
-        String birthday =" \u099C\u09A8\u09CD\u09AE\u09A6\u09BF\u09A8";
-        String mohor = " \u09AE\u09CB\u09B9\u09B0";
+        String su = "\u09B6\u09C1";
+        char vo = '\u09AD';
+        char jj ='\u099C';
+        String nm = "\u09A8\u09CD\u09AE";
+        String di = "\u09A6\u09BF";
+        char  nn = '\u09A8';
+        String mo = "\u09AE\u09CB";
+        char ho = '\u09B9';
+        char rr = '\u09B0';
+        char space = ' ';
 
-
-        System.out.print(happy);
+        System.out.print(su);
         TimeUnit.MILLISECONDS.sleep(500);
-        System.out.print(birthday);
+        System.out.print(vo);
         TimeUnit.MILLISECONDS.sleep(500);
-        System.out.print(mohor);
+        System.out.print(space);
+        TimeUnit.MILLISECONDS.sleep(500);
+        System.out.print(jj);
+        TimeUnit.MILLISECONDS.sleep(500);
+        System.out.print(nm);
+        TimeUnit.MILLISECONDS.sleep(500);
+        System.out.print(di);
+        TimeUnit.MILLISECONDS.sleep(500);
+        System.out.print(nn);
+        TimeUnit.MILLISECONDS.sleep(500);
+        System.out.print(space);
+        TimeUnit.MILLISECONDS.sleep(500);
+        System.out.print(mo);
+        TimeUnit.MILLISECONDS.sleep(500);
+        System.out.print(ho);
+        TimeUnit.MILLISECONDS.sleep(500);
+        System.out.println(rr);
     }
 }
