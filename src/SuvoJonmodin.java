cb9aa9e4bf51483e39457cf8356602029f34054c
Shayed Hasan Shakib <shayedhasanshakib@gmail.com>
2021-10-29 07:03:15 +0600

    added time delay with three separable string

--- src/SuvoJonmodin.java
+++ src/SuvoJonmodin.java
@@ -1,10 +1,19 @@
+import java.util.concurrent.TimeUnit;
+
 public class SuvoJonmodin {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
 
-        String hbd = "\t\t\u09B6\u09C1\u09AD \u099C\u09A8\u09CD\u09AE\u09A6\u09BF\u09A8 \u09AE\u09CB\u09B9\u09B0";
+        String happy = "\u09B6\u09C1\u09AD";
+        String birthday =" \u099C\u09A8\u09CD\u09AE\u09A6\u09BF\u09A8";
+        String mohor = " \u09AE\u09CB\u09B9\u09B0";
 
-        System.out.println(hbd);
+
+        System.out.print(happy);
+        TimeUnit.MILLISECONDS.sleep(500);
+        System.out.print(birthday);
+        TimeUnit.MILLISECONDS.sleep(500);
+        System.out.print(mohor);
     }
 }
