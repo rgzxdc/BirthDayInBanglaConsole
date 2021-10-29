3a786e3ba25a58467b707c607e8f4461cc7d71c3
Shayed Hasan Shakib <shayedhasanshakib@gmail.com>
2021-10-29 06:56:08 +0600

    single string , works fine

--- src/SuvoJonmodin.java
+++ src/SuvoJonmodin.java
@@ -2,7 +2,9 @@ public class SuvoJonmodin {
 
     public static void main(String[] args) {
 
-        System.out.println("\t\tHello");
-        //I need bangla letter unicode value
+
+        String hbd = "\t\t\u09B6\u09C1\u09AD \u099C\u09A8\u09CD\u09AE\u09A6\u09BF\u09A8 \u09AE\u09CB\u09B9\u09B0";
+
+        System.out.println(hbd);
     }
 }
